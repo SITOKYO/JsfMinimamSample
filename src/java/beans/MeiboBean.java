8d1cadd31dfc73f5ea63a8712dd887231b202203
kinopp <kinopp@192.168.11.2>
2016-08-30 22:25:13 +0900

    リダイレクトの記述追加

--- src/java/beans/MeiboBean.java
+++ src/java/beans/MeiboBean.java
@@ -1,6 +1,8 @@
 package beans;
 
 import javax.enterprise.context.RequestScoped;
+import javax.enterprise.context.SessionScoped;
+import javax.faces.context.FacesContext;
 import javax.inject.Named;
 
 @Named
@@ -11,8 +13,14 @@ public class MeiboBean {
     private String name;
 
     public String next() {
+        
+        //FacesContext facesContext = FacesContext.getCurrentInstance();
+        //facesContext.getExternalContext().getFlash().put("number", this.number);
+        //facesContext.getExternalContext().getFlash().put("name", this.name);
+        
         System.out.println("★number=" + this.number + "/ name=" + this.name);
-        return "output.xhtml"; // 次に表示するウェブページ
+        //return "output.xhtml?faces-redirect=true"; // 次に表示するウェブページ
+        return "output.xhtml";
     }
 
     public String getName() {
